dbb0c9a8f98c46b42e4fb0d05176e400cca86a01
OUSSEMA GUERAMI <oussema.guerami@insat.ucar.tn>
2025-05-02 15:35:20 +0100

    migrate to gurobi

--- app.py
+++ app.py
@@ -1,41 +1,55 @@
 from flask import Flask, request, jsonify
 from ortools.linear_solver import pywraplp
 from flask_cors import CORS
+import gurobipy as gp
 
 app = Flask(__name__)
 CORS(app)
 
 #Max flow problem Algorithm , Don't touch this
-def max_flow_lp(graph, capacities, source, sink):
-    solver = pywraplp.Solver.CreateSolver('CBC')
+from gurobipy import Model, GRB
+
+def max_flow_lp_gurobi(graph, capacities, source, sink):
+    model = Model("max_flow")
+
+    # Créer les variables de flux
     flow = {}
     for (i, j) in graph:
-        flow[(i, j)] = solver.NumVar(0, capacities[f"{i},{j}"], f'f_{i}_{j}')
+        cap = capacities[f"{i},{j}"]
+        flow[(i, j)] = model.addVar(lb=0, ub=cap, vtype=GRB.CONTINUOUS, name=f'f_{i}_{j}')
+
+    model.update()
+
+    # Contraintes : pas d'entrée dans le source
     for (i, j) in graph:
         if j == source:
-            solver.Add(flow[(i, j)] == 0)
+            model.addConstr(flow[(i, j)] == 0)
 
+    # Conservation du flux (Kirchhoff) pour tous les nœuds sauf source et puits
     nodes = set(i for (i, j) in graph) | set(j for (i, j) in graph)
     for node in nodes:
         if node == source or node == sink:
             continue
         inflow = [flow[(i, j)] for (i, j) in graph if j == node]
         outflow = [flow[(i, j)] for (i, j) in graph if i == node]
-        solver.Add(solver.Sum(inflow) == solver.Sum(outflow))
+        model.addConstr(sum(inflow) == sum(outflow))
 
+    # Objectif : maximiser le flux total sortant du source
     total_out = [flow[(i, j)] for (i, j) in graph if i == source]
-    solver.Maximize(solver.Sum(total_out))
+    model.setObjective(sum(total_out), GRB.MAXIMIZE)
 
-    status = solver.Solve()
+    # Résolution
+    model.optimize()
 
-    if status == pywraplp.Solver.OPTIMAL:
+    if model.status == GRB.OPTIMAL:
         result = {
-            "max_flow": solver.Objective().Value(),
-            "flows": {f"{i},{j}": flow[(i, j)].solution_value() for (i, j) in graph}
+            "max_flow": model.objVal,
+            "flows": {f"{i},{j}": flow[(i, j)].X for (i, j) in graph}
         }
         return result
     else:
         return {"error": "No optimal solution found"}
+
 
 # end of algorithm.
 
@@ -53,7 +67,7 @@ def run_max_flow():
     source = data['source']
     sink = data['sink']
 
-    result = max_flow_lp(graph, capacities, source, sink)
+    result = max_flow_lp_gurobi(graph, capacities, source, sink)
     return jsonify(result)
 
 if __name__ == '__main__':
